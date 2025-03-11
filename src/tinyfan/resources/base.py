b31362211eb30d64815a342c47744444cd099974
ec.song <ec.song@ejn.gg>
2025-03-11 16:10:31 +0900

    fix: resource config refs not working

--- src/tinyfan/resources/base.py
+++ src/tinyfan/resources/base.py
@@ -1,10 +1,11 @@
 from ..config import Config, ConfigValue
+import inspect
 
 
 class ResourceBase:
     def get_refs(self) -> set[Config | ConfigValue]:
         refs = set()
-        for _, value in vars(self).items():
+        for _, value in inspect.getmembers(self):
             if isinstance(value, (Config, ConfigValue)):  # or a common base 'Reference'
                 refs.add(value)
             elif isinstance(value, ResourceBase):
